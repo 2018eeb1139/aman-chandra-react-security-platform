3e7bc87eb1d67c52ca1e848a3e348ddb68e1a419
2018eeb1139 <amanpratapchandra1999@gmail.com>
2026-03-04 09:20:05 +0530

    added sidebar routing pages

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,30 @@
+import { Layout } from "@/components/Layout";
+
+const Projects = () => {
+  return (
+    <Layout
+      title="Projects"
+      breadcrumbs={["Scans", "Private Assets"]}
+      actions={[
+        {
+          label: "Export Report",
+          variant: "outline",
+          onClick: () => {
+            toast.success("Exporting report");
+          },
+        },
+        {
+          label: "Stop Scan",
+          variant: "destructive",
+          onClick: () => {
+            toast.error("Scanning stopped");
+          },
+        },
+      ]}
+    >
+      <div className="m-2">Projects</div>
+    </Layout>
+  );
+};
+
+export default Projects;
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,30 @@
+import { Layout } from "@/components/Layout";
+
+const Settings = () => {
+  return (
+    <Layout
+      title="Projects"
+      breadcrumbs={["Scans", "Private Assets"]}
+      actions={[
+        {
+          label: "Export Report",
+          variant: "outline",
+          onClick: () => {
+            toast.success("Exporting report");
+          },
+        },
+        {
+          label: "Stop Scan",
+          variant: "destructive",
+          onClick: () => {
+            toast.error("Scanning stopped");
+          },
+        },
+      ]}
+    >
+      <div className="m-2">Settings</div>
+    </Layout>
+  );
+};
+
+export default Settings;
